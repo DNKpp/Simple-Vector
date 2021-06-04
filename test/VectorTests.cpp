2746011ff6e6defadc0f1596f0b14fbc8cddd6a9
DNKpp <DNKpp2011@gmail.com>
2021-06-05 01:36:41 +0200

    re-enable accidentally disabled test
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- test/VectorTests.cpp
+++ test/VectorTests.cpp
@@ -112,8 +112,8 @@ TEMPLATE_TEST_CASE_SIG
 	"Vector types should be explicitly convertible between different dimension sizes.",
 	"[vector][construction]",
 	((class TSource, class TTarget, auto VExpectedRange), TSource, TTarget, VExpectedRange),
-	(Vector<int, 3>, Vector<int, 2>, std::to_array({ 1, 2 })) /*,
-	(Vector<int, 3>, Vector<int, 5>, std::to_array({ 1, 2, 3, 0, 0 }))*/
+	(Vector<int, 3>, Vector<int, 2>, std::to_array({ 1, 2 })),
+	(Vector<int, 3>, Vector<int, 5>, std::to_array({ 1, 2, 3, 0, 0 }))
 )
 #pragma warning(default: 26444)
 {
