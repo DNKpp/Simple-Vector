11a4a3940d33f8c94f9b1ec8e82d898e835f16a1
DNKpp <DNKpp2011@gmail.com>
2021-06-05 21:32:27 +0200

    Concepts.hpp: extend docs
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Vector/Concepts.hpp
+++ include/Simple-Vector/Concepts.hpp
@@ -17,73 +17,134 @@ namespace sl::vec
 	 * @{
 	 */
 
+	/**
+	 * \concept add_assignable
+	 * \brief Checks if the given type can be used in mutable plus operations.
+	 * \tparam TFrom Right-hand-side type of operation
+	 * \tparam TTo Left-hand-side type of operation
+	 */
 	template <class TFrom, class TTo = TFrom>
 	concept add_assignable = requires(TTo lhs, TFrom rhs)
 	{
 		{ lhs += rhs };
 	};
 
+	/**
+	 * \concept sub_assignable
+	 * \brief Checks if the given type can be used in mutable minus operations.
+	 * \tparam TFrom Right-hand-side type of operation
+	 * \tparam TTo Left-hand-side type of operation
+	 */
 	template <class TFrom, class TTo = TFrom>
 	concept sub_assignable = requires(TTo lhs, TFrom rhs)
 	{
 		{ lhs -= rhs };
 	};
 
+	/**
+	 * \concept mul_assignable
+	 * \brief Checks if the given type can be used in mutable multiplication operations.
+	 * \tparam TFrom Right-hand-side type of operation
+	 * \tparam TTo Left-hand-side type of operation
+	 */
 	template <class TFrom, class TTo = TFrom>
 	concept mul_assignable = requires(TTo lhs, TFrom rhs)
 	{
 		{ lhs *= rhs };
 	};
 
+	/**
+	 * \concept div_assignable
+	 * \brief Checks if the given type can be used in mutable division operations.
+	 * \tparam TFrom Right-hand-side type of operation
+	 * \tparam TTo Left-hand-side type of operation
+	 */
 	template <class TFrom, class TTo = TFrom>
 	concept div_assignable = requires(TTo lhs, TFrom rhs)
 	{
 		{ lhs /= rhs };
 	};
 
+	/**
+	 * \concept mod_assignable
+	 * \brief Checks if the given type can be used in mutable modulo operations.
+	 * \tparam TFrom Right-hand-side type of operation
+	 * \tparam TTo Left-hand-side type of operation
+	 */
 	template <class TFrom, class TTo = TFrom>
 	concept mod_assignable = requires(TTo lhs, TFrom rhs)
 	{
 		{ lhs %= rhs };
 	};
 
+	/**
+	 * \concept addable
+	 * \brief Checks if the given type can be used in immutable plus operations.
+	 * \tparam TFrom Right-hand-side type of operation
+	 * \tparam TTo Left-hand-side type of operation and expected result type
+	 */
 	template <class TFrom, class TTo = TFrom>
 	concept addable = requires(TTo lhs, TFrom rhs)
 	{
 		{ lhs + rhs } -> std::convertible_to<TTo>;
 	};
 
+	/**
+	 * \concept subable
+	 * \brief Checks if the given type can be used in immutable minus operations.
+	 * \tparam TFrom Right-hand-side type of operation
+	 * \tparam TTo Left-hand-side type of operation and expected result type
+	 */
 	template <class TFrom, class TTo = TFrom>
 	concept subable = requires(TTo lhs, TFrom rhs)
 	{
 		{ lhs - rhs } -> std::convertible_to<TTo>;
 	};
 
+	/**
+	 * \concept mulable
+	 * \brief Checks if the given type can be used in immutable multiplication operations.
+	 * \tparam TFrom Right-hand-side type of operation
+	 * \tparam TTo Left-hand-side type of operation and expected result type
+	 */
 	template <class TFrom, class TTo = TFrom>
 	concept mulable = requires(TTo lhs, TFrom rhs)
 	{
 		{ lhs * rhs } -> std::convertible_to<TTo>;
 	};
 
+	/**
+	 * \concept divable
+	 * \brief Checks if the given type can be used in immutable division operations.
+	 * \tparam TFrom Right-hand-side type of operation
+	 * \tparam TTo Left-hand-side type of operation and expected result type
+	 */
 	template <class TFrom, class TTo = TFrom>
 	concept divable = requires(TTo lhs, TFrom rhs)
 	{
 		{ lhs / rhs } -> std::convertible_to<TTo>;
 	};
 
+	/**
+	 * \concept modable
+	 * \brief Checks if the given type can be used in immutable module operations.
+	 * \tparam TFrom Right-hand-side type of operation
+	 * \tparam TTo Left-hand-side type of operation and expected result type
+	 */
 	template <class TFrom, class TTo = TFrom>
 	concept modable = requires(TTo lhs, TFrom rhs)
 	{
 		{ lhs % rhs } -> std::convertible_to<TTo>;
 	};
+
 	/**
 	 * \concept binary_invokable_with_all_overloads_implicit_convertible_to
 	 * \brief Checks if binary operation has all four overloads (one for each parameter constellation) and if all of those
 	 * overloads are implicitly convertible to the result type.
 	 * \tparam TBinaryOp Tpy of binary operation to check
 	 * \tparam TResult Expected type to which each overload should be implicitly convertible to
-	 * \param TArg1 Type of first parameter
-	 * \param TArg2 Type of second parameter
+	 * \tparam TArg1 Type of first parameter
+	 * \tparam TArg2 Type of second parameter
 	 */
 	template <class TBinaryOp, class TResult, class TArg1, class TArg2>
 	concept binary_invokable_with_all_overloads_implicit_convertible_to =
@@ -92,6 +153,11 @@ namespace sl::vec
 	std::is_convertible_v<std::invoke_result_t<TBinaryOp&, TArg2, TArg1>, TResult> &&
 	std::is_convertible_v<std::invoke_result_t<TBinaryOp&, TArg2, TArg2>, TResult>;
 
+	/**
+	 * \concept value_type
+	 * \brief Checks if the given type is regular and can be used in common arithmetically operations.
+	 * \tparam T Type to check
+	 */
 	template <class T>
 	concept value_type = std::regular<std::remove_cvref_t<T>> &&
 						addable<std::remove_cvref_t<T>> &&
